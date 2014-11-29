211a7c34bc84c0a5a5a807cde3fea4eea2b630db
siimsoni <dev@siimsoni.ee>
2014-11-30 01:06:13 +0200

    Added profile view to see helpers

--- application/models/helper_profile_model.php
+++ application/models/helper_profile_model.php
@@ -24,4 +24,16 @@ class Helper_Profile_Model extends CI_Model {
         $this->db->where('deleted_at IS NULL', null, false);
         return $this->db->count_all_results() > 0;
     }
+
+    public function getProfileForUser($userId) {
+        $this->db->from(self::TABLE);
+        $this->db->where(self::TABLE . '.user_id', $userId);
+        $this->db->where(self::TABLE . '.deleted_at IS NULL', null, false);
+        $this->db->join('users', 'users.user_id = '.self::TABLE.'.user_id');
+        $query = $this->db->get();
+        foreach($query->result() as $row) {
+            return $row;
+        }
+        return false;
+    }
 }
