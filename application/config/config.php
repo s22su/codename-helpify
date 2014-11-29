ea065a769faf308c2ea2672ce4f4575c6c34c1fa
siimsoni <dev@siimsoni.ee>
2014-11-29 10:49:53 +0200

    Facebook credentials + add www to url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://helpify.com';
+$config['base_url']	= 'http://www.helpify.com';
 
 /*
 |--------------------------------------------------------------------------
