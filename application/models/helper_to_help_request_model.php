246eb190b1cbc5d2d70b407c84266aaa8c4aeb92
siimsoni <dev@siimsoni.ee>
2014-11-29 23:19:12 +0200

    Add help request to helper association

---
+++ application/models/helper_to_help_request_model.php
@@ -0,0 +1,37 @@
+<?php
+class Helper_To_Help_Request_Model extends CI_Model {
+    const TABLE = 'helper_to_help_request';
+
+    var $id;
+    var $do_help_user_id;
+    var $help_request_id;
+    var $description;
+    var $accepted;
+    var $created_at;
+
+    public function insert($data) {
+        $this->db->insert(self::TABLE, $data);
+    }
+
+    public function listAcceptedHelpOffersByHelpRequest($requestId) {
+        $this->db->from(self::TABLE);
+        $this->db->where('help_request_id', $requestId);
+        $this->db->where('accepted', '1');
+        $results = array();
+        foreach($this->db->get()->result() as $row) {
+            $results[] = $row;
+        }
+        return $results;
+    }
+
+    public function listUnacceptedHelpOffersByHelpRequest($requestId) {
+        $this->db->from(self::TABLE);
+        $this->db->where('help_request_id', $requestId);
+        $this->db->where('accepted', '0');
+        $results = array();
+        foreach($this->db->get()->result() as $row) {
+            $results[] = $row;
+        }
+        return $results;
+    }
+}
